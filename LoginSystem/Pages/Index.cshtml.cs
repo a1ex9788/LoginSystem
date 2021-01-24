80fe7402aed0e7160f0071a61abda2f8f12a20b9
Alejandro <alejandrocarrion1999@gmail.com>
2021-01-24 14:07:38 +0100

    Hacer funcionar los botones.

--- LoginSystem/Pages/Index.cshtml.cs
+++ LoginSystem/Pages/Index.cshtml.cs
@@ -1,16 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace LoginSystem.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        [TempData]
+        public string Username { get; set; }
 
         public void OnGet()
         {
--- LoginSystem/Pages/Register.cshtml.cs
+++ LoginSystem/Pages/Register.cshtml.cs
@@ -9,8 +9,34 @@ namespace LoginSystem.Pages
 {
     public class RegisterModel : PageModel
     {
+        [BindProperty]
+        public LoginInformation loginInformation { get; set; }
+
+        [TempData]
+        public string Username { get; set; }
+
         public void OnGet()
         {
         }
+
+        public IActionResult OnPostCancel()
+        {
+            return RedirectToPage("Index");
+        }
+
+        public IActionResult OnPostRegister()
+        {
+            Username = loginInformation.Username;
+            return RedirectToPage("Index");
+        }
+    }
+
+    public class LoginInformation
+    {
+        public string Name { get; set; }
+        public string Surnames { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string RepeatedPassword { get; set; }
     }
 }
