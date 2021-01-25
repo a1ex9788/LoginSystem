53388dbb69e1b669f6dc1c6a1c7364a0cc340967
Alejandro <alejandrocarrion1999@gmail.com>
2021-01-25 18:18:21 +0100

    Añadir LogicManager al Scope.

--- LoginSystem/Startup.cs
+++ LoginSystem/Startup.cs
@@ -1,3 +1,4 @@
+using LoginSystem.Logic;
 using LoginSystem.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -19,6 +20,7 @@ namespace LoginSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<LogicManager>();
             services.AddDbContext<LoginSystemDBContext>();
             services.AddRazorPages();
         }
