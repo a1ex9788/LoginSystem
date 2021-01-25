9b828af5f20dc9a49f5854db0a266bf4810b29cb
Alejandro <alejandrocarrion1999@gmail.com>
2021-01-25 19:29:35 +0100

    Implementación controllers.

--- LoginSystem/Controllers/LoginController.cs
+++ LoginSystem/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using LoginSystem.Logic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +12,20 @@ namespace LoginSystem.Controllers
     [Route("api/[controller]")]
     public class LoginController : ControllerBase
     {
+        private readonly ILogger<LoginController> logger;
         private readonly LogicManager logicManager;
 
-        public LoginController(LogicManager logicManager)
+        public LoginController(LogicManager logicManager, ILogger<LoginController> logger)
         {
+            this.logger = logger;
             this.logicManager = logicManager;
         }
 
-        [HttpGet]
+        [HttpGet("{username}/{password}")]
         public bool Login(string username, string password)
         {
+            logger.LogInformation($"Request received: username={username}, password={password}");
+
             return logicManager.UserExists(username, password);
         }
     }
--- LoginSystem/Controllers/RegisterController.cs
+++ LoginSystem/Controllers/RegisterController.cs
@@ -9,19 +9,23 @@ using Microsoft.Extensions.Logging;
 namespace LoginSystem.Controllers
 {
     [ApiController]
-    [Route("api/controller")]
+    [Route("api/[controller]")]
     public class RegisterController : ControllerBase
     {
+        private readonly ILogger<RegisterController> logger;
         private readonly LogicManager logicManager;
 
-        public RegisterController(LogicManager logicManager)
+        public RegisterController(LogicManager logicManager, ILogger<RegisterController> logger)
         {
+            this.logger = logger;
             this.logicManager = logicManager;
         }
 
-        [HttpGet]
+        [HttpGet("{name}/{surnames}/{username}/{password}")]
         public bool Register(string name, string surnames, string username, string password)
         {
+            logger.LogInformation($"Request received: name={name}, surnames={surnames}, username={username}, password={password}");
+
             return logicManager.RegisterUser(name, surnames, username, password);
         }
     }
